2d4383b679cbdda12fad3d133f5ba1b3c32f3b2c
luca-de-bar <lucadebar33@gmail.com>
2024-05-17 16:21:05 +0200

    add : snack 10

--- snack10/main.js
+++ snack10/main.js
@@ -1,2 +1,24 @@
 'use strict';
 
+//Inizializzo array vuoto
+const myNumbers = [];
+window.alert('Nel seguente programma, se inserisci 2 volte lo stesso numero, il programma terminerà')
+
+//Creo un ciclo for infinito
+for (let i = false; i = true; i++){
+    
+    const userInput = parseInt(prompt('Inserisci un numero'));
+    
+    //Se input utente in array, blocco il ciclo altrimenti aggiungo input all'array.
+    if (myNumbers.includes(userInput)) {
+
+        console.log('Non puoi scrivere lo stesso numero 2 volte!!')
+        break
+
+    } else {
+        myNumbers.push(userInput);
+    }
+    
+}
+
+//Non valido l'input..già fatto negli snack precedenti :)
--- snack10/readme.md
+++ snack10/readme.md
@@ -1,4 +1,8 @@
 ## Snack 10
 
--Scrivere un programma a cui venga chiesto all'utente di inserire numeri uno dopo l'altro.
--Quando l'utente scriverà 2 volte consecutive lo stesso numero allora il programma dovrà terminare.
+- Scrivere un programma a cui venga chiesto all'utente di inserire numeri uno dopo l'altro.
+- Quando l'utente scriverà 2 volte consecutive lo stesso numero allora il programma dovrà terminare.
+
+## Cosa ho imparato?
+- Ho consolidato le conoscenze sugli array e la "manipolazione" degli stessi.
+- Ho imparato a interrompere un ciclo utilizzando break.
